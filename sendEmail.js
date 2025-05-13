0e4c843ebce67783ca8a8e0f378615df599dec1f
Jose Ignacio Opazo  Lopez <jose.opazo@falp.org>
2025-05-13 15:57:26 -0400

    Agrega script para enviar notificaciones por correo

---
+++ sendEmail.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+
+const status = process.argv[2]; // "success" o "failure"
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  }
+});
+
+const subject = status === "success"
+  ? "✅ Pipeline exitosa: Monitoreo Portal 1.0"
+  : "❌ Falló la pipeline: Monitoreo Portal 1.0";
+
+const body = status === "success"
+  ? "El monitoreo automatizado del Portal 1.0 fue exitoso.."
+  : "Falló el monitoreo automático. Revisa los logs: https://github.com/${{ github.repository }}/actions/runs/${{ github.run_id }}";
+
+transporter.sendMail({
+  from: `"QA Monitor" <${process.env.SMTP_USER}>`,
+  to: "qa@falp.org,desarrollo@falp.org",
+  subject,
+  text: body
+}, (error, info) => {
+  if (error) {
+    console.log("❌ Error al enviar correo:", error);
+    process.exit(1);
+  } else {
+    console.log("✅ Correo enviado:", info.response);
+  }
+});
